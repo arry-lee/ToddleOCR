d71b12966bf9da91f870e93236f526ca7e3e209e
arry-lee <arry_lee@qq.com>
2023-08-08 13:59:36 +0800

    replace Polygon with shapely.geometry.Polygon

--- ppocr/utils/e2e_metric/Deteval.py
+++ ppocr/utils/e2e_metric/Deteval.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import Polygon as plg
+from shapely.geometry import Polygon as plg
 import numpy as np
 import scipy.io as io
 
