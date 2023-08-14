d6b222cbbafd8b2cbbdce65b8970c366d113bfbe
arry-lee <arry_lee@qq.com>
2023-08-14 12:25:20 +0800

    replace xyxyxyxy with xy4

--- ppocr/metrics/table_metric.py
+++ ppocr/metrics/table_metric.py
@@ -1,49 +1,30 @@
-# copyright (c) 2020 PaddlePaddle Authors. All Rights Reserve.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-import numpy as np
 from ppocr.metrics.det_metric import DetMetric
+
+__all__ = ['TableStructureMetric', 'TableMetric']
 
 
 class TableStructureMetric:
-    def __init__(self, main_indicator="acc", eps=1e-6, del_thead_tbody=False, **kwargs):
+
+    def __init__(self, main_indicator='acc', eps=1e-06, del_thead_tbody=False, **kwargs):
         self.main_indicator = main_indicator
         self.eps = eps
         self.del_thead_tbody = del_thead_tbody
         self.reset()
 
     def __call__(self, pred_label, batch=None, *args, **kwargs):
-        preds, labels = pred_label
-        pred_structure_batch_list = preds["structure_batch_list"]
-        gt_structure_batch_list = labels["structure_batch_list"]
+        (preds, labels) = pred_label
+        pred_structure_batch_list = preds['structure_batch_list']
+        gt_structure_batch_list = labels['structure_batch_list']
         correct_num = 0
         all_num = 0
-        for (pred, pred_conf), target in zip(pred_structure_batch_list, gt_structure_batch_list):
-            pred_str = "".join(pred)
-            target_str = "".join(target)
+        for ((pred, pred_conf), target) in zip(pred_structure_batch_list, gt_structure_batch_list):
+            pred_str = ''.join(pred)
+            target_str = ''.join(target)
             if self.del_thead_tbody:
-                pred_str = (
-                    pred_str.replace("<thead>", "")
-                    .replace("</thead>", "")
-                    .replace("<tbody>", "")
-                    .replace("</tbody>", "")
-                )
-                target_str = (
-                    target_str.replace("<thead>", "")
-                    .replace("</thead>", "")
-                    .replace("<tbody>", "")
-                    .replace("</tbody>", "")
-                )
+                pred_str = pred_str.replace('<thead>', '').replace('</thead>', '').replace('<tbody>', '').replace(
+                    '</tbody>', '')
+                target_str = target_str.replace('<thead>', '').replace('</thead>', '').replace('<tbody>', '').replace(
+                    '</tbody>', '')
             if pred_str == target_str:
                 correct_num += 1
             all_num += 1
@@ -51,14 +32,9 @@ class TableStructureMetric:
         self.all_num += all_num
 
     def get_metric(self):
-        """
-        return metrics {
-                 'acc': 0,
-            }
-        """
         acc = 1.0 * self.correct_num / (self.all_num + self.eps)
         self.reset()
-        return {"acc": acc}
+        return {'acc': acc}
 
     def reset(self):
         self.correct_num = 0
@@ -69,15 +45,9 @@ class TableStructureMetric:
 
 
 class TableMetric:
-    def __init__(
-        self, main_indicator="acc", compute_bbox_metric=False, box_format="xyxy", del_thead_tbody=False, **kwargs
-    ):
-        """
 
-        @param sub_metrics: configs of sub_metric
-        @param main_matric: main_matric for save best_model
-        @param kwargs:
-        """
+    def __init__(self, main_indicator='acc', compute_bbox_metric=False, box_format='xyxy', del_thead_tbody=False,
+                 **kwargs):
         self.structure_metric = TableStructureMetric(del_thead_tbody=del_thead_tbody)
         self.bbox_metric = DetMetric() if compute_bbox_metric else None
         self.main_indicator = main_indicator
@@ -93,23 +63,18 @@ class TableMetric:
         pred_bbox_batch_list = []
         gt_ignore_tags_batch_list = []
         gt_bbox_batch_list = []
-        preds, labels = pred_label
-
-        batch_num = len(preds["bbox_batch_list"])
+        (preds, labels) = pred_label
+        batch_num = len(preds['bbox_batch_list'])
         for batch_idx in range(batch_num):
-            # pred
-            pred_bbox_list = [self.format_box(pred_box) for pred_box in preds["bbox_batch_list"][batch_idx]]
-            pred_bbox_batch_list.append({"points": pred_bbox_list})
-
-            # gt
+            pred_bbox_list = [self.format_box(pred_box) for pred_box in preds['bbox_batch_list'][batch_idx]]
+            pred_bbox_batch_list.append({'points': pred_bbox_list})
             gt_bbox_list = []
             gt_ignore_tags_list = []
-            for gt_box in labels["bbox_batch_list"][batch_idx]:
+            for gt_box in labels['bbox_batch_list'][batch_idx]:
                 gt_bbox_list.append(self.format_box(gt_box))
                 gt_ignore_tags_list.append(0)
             gt_bbox_batch_list.append(gt_bbox_list)
             gt_ignore_tags_batch_list.append(gt_ignore_tags_list)
-
         return [pred_bbox_batch_list, [0, 0, gt_bbox_batch_list, gt_ignore_tags_batch_list]]
 
     def get_metric(self):
@@ -120,11 +85,11 @@ class TableMetric:
         if self.main_indicator == self.bbox_metric.main_indicator:
             output = bbox_metric
             for sub_key in structure_metric:
-                output["structure_metric_{}".format(sub_key)] = structure_metric[sub_key]
+                output['structure_metric_{}'.format(sub_key)] = structure_metric[sub_key]
         else:
             output = structure_metric
             for sub_key in bbox_metric:
-                output["bbox_metric_{}".format(sub_key)] = bbox_metric[sub_key]
+                output['bbox_metric_{}'.format(sub_key)] = bbox_metric[sub_key]
         return output
 
     def reset(self):
@@ -133,14 +98,14 @@ class TableMetric:
             self.bbox_metric.reset()
 
     def format_box(self, box):
-        if self.box_format == "xyxy":
-            x1, y1, x2, y2 = box
+        if self.box_format == 'xyxy':
+            (x1, y1, x2, y2) = box
             box = [[x1, y1], [x2, y1], [x2, y2], [x1, y2]]
-        elif self.box_format == "xywh":
-            x, y, w, h = box
-            x1, y1, x2, y2 = x - w // 2, y - h // 2, x + w // 2, y + h // 2
+        elif self.box_format == 'xywh':
+            (x, y, w, h) = box
+            (x1, y1, x2, y2) = (x - w // 2, y - h // 2, x + w // 2, y + h // 2)
             box = [[x1, y1], [x2, y1], [x2, y2], [x1, y2]]
-        elif self.box_format == "xyxyxyxy":
-            x1, y1, x2, y2, x3, y3, x4, y4 = box
+        elif self.box_format == 'xy4':
+            (x1, y1, x2, y2, x3, y3, x4, y4) = box
             box = [[x1, y1], [x2, y2], [x3, y3], [x4, y4]]
         return box
