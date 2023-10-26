5443f34f813238d6bcd692540956275327d514c0
arry-lee <arry_lee@qq.com>
2023-10-26 18:28:14 +0800

    note mobilenet v3

--- ptocr/modules/backbones/mobilenetv3/rec_mobilenet_v3.py
+++ ptocr/modules/backbones/mobilenetv3/rec_mobilenet_v3.py
@@ -6,15 +6,16 @@ __all__ = ["MobileNetV3Rec"]
 
 
 class MobileNetV3Rec(nn.Module):
+
     def __init__(
-        self,
-        in_channels=3,
-        model_name="small",
-        scale=0.5,
-        large_stride=None,
-        small_stride=None,
-        disable_se=False,
-        **kwargs
+            self,
+            in_channels=3,
+            model_name="small",
+            scale=0.5,
+            large_stride: list[int] = None,
+            small_stride: list[int] = None,
+            disable_se=False,
+            **kwargs
     ):
         super().__init__()
         self.disable_se = disable_se
@@ -23,8 +24,6 @@ class MobileNetV3Rec(nn.Module):
         if large_stride is None:
             large_stride = [1, 2, 2, 2]
 
-        assert isinstance(large_stride, list), "large_stride type must " "be list but got {}".format(type(large_stride))
-        assert isinstance(small_stride, list), "small_stride type must " "be list but got {}".format(type(small_stride))
         assert len(large_stride) == 4, "large_stride length must be " "4 but got {}".format(len(large_stride))
         assert len(small_stride) == 4, "small_stride length must be " "4 but got {}".format(len(small_stride))
 
