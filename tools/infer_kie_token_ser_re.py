be93b4c19191be49bb3dfb3f1682a2f8d9f1bf15
arry-lee <arry_lee@qq.com>
2023-10-27 14:52:23 +0800

    rm

--- tools/infer_kie_token_ser_re.py
+++ tools/infer_kie_token_ser_re.py
@@ -1,21 +1,3 @@
-# Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-
-
-
-
 import os
 import sys
 
@@ -156,7 +138,6 @@ def preprocess():
 
     logger = get_logger()
 
-    # check if set use_gpu=True in paddlepaddle cpu version
     use_gpu = config["Global"]["use_gpu"]
 
     device = "gpu:{}".format(dist.ParallelEnv().dev_id) if use_gpu else "cpu"
